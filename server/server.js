db7ba7c0c0f42c09ec4a0cd858d0160d264e5eb7
SuaveDonny <dondre1911@gmail.com>
2026-05-23 22:33:21 -0500

    Debug static path

--- server/server.js
+++ server/server.js
@@ -557,6 +557,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log("Static path:", path.join(__dirname, "../frontend/dist"));
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("*", (req, res) => { res.sendFile(path.join(__dirname, "../frontend/dist/index.html")); });
 
