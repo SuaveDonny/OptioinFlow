e5cc9dbb4920811a9eed56a6c5c44bc4dc130c8d
SuaveDonny <dondre1911@gmail.com>
2026-05-24 23:24:57 -0500

    Dynamic API URL via query param

--- server/server.js
+++ server/server.js
@@ -576,7 +576,7 @@ app.get("*", (req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on http://localhost:" + PORT);
   console.log("Sections:", Object.keys(SECTIONS).join(", "));
   console.log("Timeframes:", Object.keys(TIMEFRAMES).join(", "));
